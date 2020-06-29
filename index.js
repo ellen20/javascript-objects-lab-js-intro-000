1d43120852219909af5e23a65ca880da7aad827b
jingling jin <jljin0524@gmail.com>
2020-06-29 18:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
   obj[key]= value
   return obj
 }
-function deleteFromObjectByKey(obj,key){
+//function deleteFromObjectByKey(obj,key){
   delete obj[key]
   return obj 
 }
